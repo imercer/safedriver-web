f7e49b4bded95ab448171f7623f816f934e12219
imercer <isaac@isaacscomputertips.com>
2015-06-14 21:34:47 +1200

    Rearranged Push Center
    Changed the way the push center works

---
+++ push/admin/index.php
@@ -0,0 +1,70 @@
+<html>
+    <head>
+<link rel="stylesheet" href="/css/metro-bootstrap.css">         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> <title>SafeDriver - Drive Safe on New Zealand Roads</title>
+        <script src="/js/jquery/jquery.min.js"></script>
+        <script src="/js/jquery/jquery.widget.min.js"></script>
+        <script src="/js/metro.min.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+    </head>
+<body class="metro" style="padding-left: 20px;">
+        <div class="container">
+        <nav class="navigation-bar dark">
+            <nav class="navigation-bar-content">
+                <h1 style="color: white">Drive Safe on New Zealand Roads</h1>
+    </nav>
+</nav>
+            <br>
+<center>
+        <a href="sendmessage.php?ref=frostice">
+    <div class="tile double bg-cyan" style="padding-left: 25px; float: left">
+        <div class="brand bg-black">
+            <span class="label fg-white" style="text-align: left">Frost and Ice Message</span>
+        </div>
+        </div>
+        </div>
+        </a>
+    <a href="sendmessage.php?ref=wet">
+    <div class="tile double bg-lightOrange" style="padding-left: 25px; float: left">
+        <div class="brand bg-black">
+            <span class="label fg-white" style="text-align: left">Wet Road Message</span>
+        </div>
+        </div>
+        </div>
+        </a>
+    <a href="sendmessage.php?ref=night">
+    <div class="tile double bg-lightOlive" style="padding-left: 25px; float: left">
+        <div class="brand bg-black">
+            <span class="label fg-white" style="text-align: left">Night Message</span>
+        </div>
+        </div>
+        </div>
+        </a>
+    <a href="sendmessage.php?ref=event">
+    <div class="tile double bg-cyan" style="padding-left: 25px; float: left">
+        <div class="brand bg-black">
+            <span class="label fg-white" style="text-align: left">Event Message</span>
+        </div>
+        </div>
+        </div>
+        </a>
+    <a href="sendmessage.php?ref=holiday">
+    <div class="tile double bg-lightOrange" style="padding-left: 25px; float: left">
+        <div class="brand bg-black">
+            <span class="label fg-white" style="text-align: left">Holiday Message</span>
+        </div>
+        </div>
+        </div>
+        </a>
+    <a href="sendmessage.php?ref=other">
+    <div class="tile double bg-red" style="padding-left: 25px; float: left">
+        <div class="brand bg-black">
+            <span class="label fg-white" style="text-align: left">Other Message</span>
+        </div>
+        </div>
+        </div>
+        </a>
+    </div>
+</center>
+</body>
