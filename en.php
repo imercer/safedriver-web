8ce8b11d74640ca49540166f4919972752e8bfe9
Isaac Mercer <isaac@isaacscomputertips.com>
2015-10-30 12:49:21 +1300

    Added Analytics

--- en.php
+++ en.php
@@ -19,6 +19,16 @@ setcookie($cookie_name, $cookie_value, time() + (2592000 * 30), "/"); // 2592000
         <link rel="stylesheet" href="https://storage.googleapis.com/code.getmdl.io/1.0.0/material.indigo-red.min.css">
         <!-- Material Design icon font -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-55227067-6', 'auto');
+  ga('send', 'pageview');
+
+</script>
     </head>
     <body>
             <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
