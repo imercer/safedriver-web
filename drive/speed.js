2cb0f6e59be849c679e6742bfd0c956252d82016
Isaac Mercer <isaac@isaacscomputertips.com>
2015-11-07 20:58:39 +1300

    when speed limit exceeded an audio alert is played
    "SLOW DOWN"

--- drive/speed.js
+++ drive/speed.js
@@ -1,17 +1,56 @@
-var id, target, options;
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+var context = new AudioContext();
+
+            var playAudio = function (buffer) {
+                var sourceBuffer = context.createBufferSource();
+
+                sourceBuffer.buffer = buffer;
+                sourceBuffer.connect(context.destination);
+                sourceBuffer.start(context.currentTime);
+                sleep(1000);
+            };
+
+            var loadAudioFile = function () {
+                var request = new XMLHttpRequest();
+
+                request.open('GET', 'slowdown.mp3', true);
+                request.responseType = 'arraybuffer';
+
+                request.onload = function () {
+                    var undecodedAudio = request.response;
+
+                    context.decodeAudioData(undecodedAudio, function (buffer) {
+                        playAudio(buffer);
+                    });
+                };
+
+                request.send();
+            };
 
 function success(pos) {
   var crd = pos.coords;
-  document.getElementById("speed").innerHTML = crd.speed + "m/s";
-    
+  var speed = crd.speed * 3.6;
+  document.getElementById("speed").innerHTML = Math.round(speed);
   if (crd.speed > target.onehundred) {
+    if (localStorage.audio) {
+    } else {
+    localStorage.audio = "present";
+    loadAudioFile();
     console.log('SLOW DOWN');
     document.getElementById("body").className = "alert";
-    document.getElementById("msg").className = "display";
     navigator.vibrate(5000);
+    }
   } else {
+    localStorage.removeItem("audio");
     document.getElementById("body").className = "non-alert";
-    document.getElementById("msg").className = "dont-display";
     navigator.vibrate(0);
   }
 }
@@ -21,7 +60,7 @@ function error(err) {
 }
 
 target = {
-  onehundred : 27.777777777778,
+  onehundred : 28.055555555556,
   eighty : 22.222222222222,
   seventy : 19.444444444444,
   sixty : 16.666666666667,
