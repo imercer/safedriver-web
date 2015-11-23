487d8416fa901e6d6d1724877f69ecb764ca6d17
Isaac Mercer <isaac@isaacscomputertips.com>
2015-11-23 19:01:54 +1300

    Removed errant targetdensity decleration

--- kr/drive/status.php
+++ kr/drive/status.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head><script src="/js/analytics.js"></script>        <script src="/drive/speed.js"></script><meta name="viewport" content="width=device-width, target-densitydpi=high-dpi, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <head><script src="/js/analytics.js"></script>        <script src="/drive/speed.js"></script><meta name="viewport" content="width=device-width,  initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" href="/css/metro-bootstrap.css"><link rel="stylesheet" href="/css/style.css"> <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>안전 운전</title> <link rel="icon" type="image/png" href="/images/icon.png"/>
         <script src="/js/jquery/jquery.min.js"></script>
         <script src="/js/jquery/jquery.widget.min.js"></script>
